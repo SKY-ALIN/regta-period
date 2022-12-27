000a2e38988dd08ccad5b581fcc177b00e217b0f
Vladimir Alinsky <vladimir@alinsky.tech>
2022-12-28 03:54:38 +0600

    Bump package version to 0.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 project = 'regta-period'
 copyright = '2022, Vladimir Alinsky'
 author = 'Vladimir Alinsky'
-release = '0.1.0'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
